5803e12e4150cc1a5ed145287630b502d8fe5d9c
Nkhata-4 <YanjaniNkhata08@outlook.com>
2026-02-01 16:46:34 +0000

    worked on q3

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -2,10 +2,6 @@
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
 
-SELECT DepartmentName, COUNT(*) AS TotalEnrolments
-FROM 
-Department JOIN Course ON Department.DepartmentId=Course.DepartmentId
-Course JOIN Enrolment ON Course.CourseId=Enrolment.CourseId
-Enrolment JOIN Student ON Enrolment.StudentId=Student.StudentId
-GROUP BY DepartmentName;
 
+
+SELECT StudentId FROM Enrolment;
