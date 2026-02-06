ef411dfe3089f10504f16e84e9602eb2536b225d
Nkhata-4 <YanjaniNkhata08@outlook.com>
2026-02-06 12:38:53 +0000

    worked on q5

--- worksheet/task2/question5.sql
+++ worksheet/task2/question5.sql
@@ -3,11 +3,12 @@
 -- StudentId, FirstName, LastName, TotalCreditsPassed
 
 
-SELECT Student.StudentId, Student.FirstName, Student.LastName, Course.Credits AS TotalCreditsPassed
+SELECT Student.StudentId, Student.FirstName, Student.LastName, SUM(Course.Credits) AS TotalCreditsPassed
 FROM
 Student JOIN Enrolment
 ON Student.StudentId = Enrolment.StudentId
 JOIN Course 
 ON Enrolment.CourseId = Course.CourseId
 WHERE Enrolment.Grade >= 40
-GROUP BY Course.CourseId;
+GROUP BY Student.StudentId;
+
